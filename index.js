da2bb96bc2f6f41c038029a96ab30aed7b113984
joaocorreiaprof <joaocorreiaprof@gmail.com>
2023-11-14 22:14:20 +0000

    Added function to play the gmae 5 rounds

--- index.js
+++ index.js
@@ -4,41 +4,64 @@ function getComputerChoice() {
   return choices[randomIndex];
 }
 
-function game() {
-  for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
-    function playRound(playerSelection, computerSelection) {
-      const playerChoice = playerSelection.toLowerCase();
-      const computerChoice = computerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+  const computerChoice = computerSelection.toLowerCase();
 
-      if (playerChoice === computerChoice) {
-        return "It's a tie!";
-      } else if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "scissors" && computerChoice === "paper")
-      ) {
-        return (
-          "You Win! " +
-          playerChoice.charAt(0).toUpperCase() +
-          playerChoice.slice(1).toLowerCase() +
-          " beats " +
-          computerChoice.charAt(0).toUpperCase() +
-          computerChoice.slice(1).toLowerCase()
-        );
-      } else {
-        return (
-          "You Lose! " +
-          computerChoice.charAt(0).toUpperCase() +
-          computerChoice.slice(1).toLowerCase() +
-          " beats " +
-          playerChoice.charAt(0).toUpperCase() +
-          playerChoice.slice(1).toLowerCase()
-        );
-      }
-    }
+  if (playerChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return (
+      "You Win! " +
+      playerChoice.charAt(0).toUpperCase() +
+      playerChoice.slice(1).toLowerCase() +
+      " beats " +
+      computerChoice.charAt(0).toUpperCase() +
+      computerChoice.slice(1).toLowerCase()
+    );
+  } else {
+    return (
+      "You Lose! " +
+      computerChoice.charAt(0).toUpperCase() +
+      computerChoice.slice(1).toLowerCase() +
+      " beats " +
+      playerChoice.charAt(0).toUpperCase() +
+      playerChoice.slice(1).toLowerCase()
+    );
   }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Choose one option: Rock, Paper or Scissors");
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerWins = 0;
+  let computerWins = 0;
+
+  for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt(
+      "Choose one option: Rock, Paper, or Scissors"
+    );
+
+    const roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+
+    if (roundResult.includes("You Win")) {
+      playerWins++;
+    } else if (roundResult.includes("You Lose")) {
+      computerWins++;
+    }
+  }
+
+  if (playerWins > computerWins) {
+    console.log("You are the overall winner!");
+  } else if (playerWins < computerWins) {
+    console.log("Computer is the overall winner!");
+  } else {
+    console.log("It's a tie overall!");
+  }
+}
+
+game();
