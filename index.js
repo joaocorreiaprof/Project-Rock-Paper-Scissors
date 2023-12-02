9acb5025ed18413ac0a8c6d42645595c5316d481
joaocorreiaprof <joaocorreiaprof@gmail.com>
2023-12-02 16:47:09 +0000

    finished js, added cs

--- index.js
+++ index.js
@@ -1,75 +1,97 @@
-//Computer will choose what to play
+// index.js
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
 
-//Function to play rounds
 function playRound(playerSelection, computerSelection) {
+  if (playerScore >= 5 || computerScore >= 5) {
+    // Game has already ended
+    return;
+  }
+
   let resultMessage = "";
-  let playerScore = 0;
-  let computerScore = 0;
+  let quoteResults = "";
 
   if (playerSelection === computerSelection) {
     resultMessage = "It's a tie!";
+    quoteResults =
+      "Terrible things are about to happen. You must not stay here, now that history is about to repeat itself";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    resultMessage =
-      "You Win! " + playerSelection + " beats " + computerSelection;
+    resultMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    quoteResults =
+      "Dobby has heard of your greatness, Sir, but never has he been asked to sit down by a wizard! Like an equal";
+    playerScore++;
   } else {
-    resultMessage =
-      "You Lose! " + computerSelection + " beats " + playerSelection;
+    resultMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    quoteResults =
+      "If you lose again, Dobby will throw himself off the topmost tower!";
+    computerScore++;
   }
-  document.querySelector(".results").innerHTML = resultMessage;
-  document.querySelector("#playerScore").innerHTML = "You " + playerScore;
-  document.querySelector("#computerScore").innerHTML =
-    "Computer " + computerScore;
 
-  return resultMessage;
+  document.querySelector(".results").innerHTML = resultMessage;
+  document.querySelector("#playerScore").innerHTML = `You ${playerScore}`;
+  document.querySelector(
+    "#computerScore"
+  ).innerHTML = `Computer ${computerScore}`;
+  document.querySelector("#quoteResults").innerHTML = quoteResults;
+
+  if (playerScore >= 5 || computerScore >= 5) {
+    endGame();
+    disableButtons();
+  }
 }
-//Initial function, where variables are declare. Its the engine
+
+function endGame() {
+  let finalMessage = "";
+  let quoteResults = "";
+
+  if (playerScore > computerScore) {
+    finalMessage = "You saved Dobby!";
+    quoteResults =
+      "Dobby: What a beautiful place to be with friends. Dobby is happy to be with his friend!";
+  } else if (playerScore < computerScore) {
+    finalMessage = "Dobby died!";
+    quoteResults =
+      "Do not pity the dead. Pity the living, and, above all, those who live without love.” ― Albus Dumbledore";
+  } else {
+    finalMessage = "Dobby lives for now. Try again!";
+    quoteResults =
+      "Dobby: If you don't make it again, Dobby will throw himself off the topmost tower!";
+  }
+
+  // Delay the finalMessage by a short amount of time to allow the UI to update
+  setTimeout(function () {
+    document.querySelector("#finalMessage").innerHTML = finalMessage;
+    document.querySelector("#quoteResults").innerHTML = quoteResults; // Use # for ID
+  }, 100);
+}
+
+function disableButtons() {
+  // Disable the buttons to prevent further clicks
+  document.getElementById("rockBtn").disabled = true;
+  document.getElementById("paperBtn").disabled = true;
+  document.getElementById("scissorsBtn").disabled = true;
+}
+
 function game() {
-  let userChoice = "";
   document.getElementById("rockBtn").addEventListener("click", function () {
-    userChoice = "rock";
-    playRound(userChoice, getComputerChoice());
+    playRound("rock", getComputerChoice());
   });
   document.getElementById("paperBtn").addEventListener("click", function () {
-    userChoice = "paper";
-    playRound(userChoice, getComputerChoice());
+    playRound("paper", getComputerChoice());
   });
   document.getElementById("scissorsBtn").addEventListener("click", function () {
-    userChoice = "scissors";
-    playRound(userChoice, getComputerChoice());
+    playRound("scissors", getComputerChoice());
   });
-
-  /*for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "";
-
-    const roundResult = playRound(let roundNumber = 1; nd(playerSelection, computerSelection);
-    console.log(roundResult);
-
-    if (roundResult.includes("You Win")) {
-      playerWins++;
-    } else if (roundResult.includes("You Lose")) {
-      computerWins++;
-    }
-  }*/
-  let finalMessage = "";
-  if (playerWins > computerWins) {
-    finalMessage = "You are the overall winner!";
-  } else if (playerWins < computerWins) {
-    finalMessage = "Computer is the overall winner!";
-  } else {
-    finalMessage = "It's a tie overall!";
-  }
 }
-
-function keepPLaying() {}
 
 game();
